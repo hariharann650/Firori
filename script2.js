6e89c6a33ccae4b30cca063268783fd2cf8b3d90
HARIHARAN P <114478496+hariharann650@users.noreply.github.com>
2024-12-29 13:59:39 +0530

    all

---
+++ script2.js
@@ -0,0 +1,31 @@
+new Swiper('.card-div', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  spaceBetween :40,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets:true
+  },
+
+  // Navigation arrows
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
+  breakpoints:{
+    0:{
+      slidesPerView:1
+    },
+  768:{
+      slidesPerView:2
+    },
+    1024:{
+      slidesPerView:3
+    },
+
+  }
+});
